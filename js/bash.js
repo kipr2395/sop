01f8a177806217e21013fadb588f88ef6dee0387
Kimberly Preston <kimberlypreston@Kimberlys-MacBook-Pro-2.local>
2017-01-30 20:30:33 -0700

    added js

---
+++ js/bash.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+		$("ul").hide();
+
+		 $(".one").click(function(){
+        $(".firstlist").show();
+        $(".one").hide();
+    });
+		 $(".two").click(function(){
+        $(".secondlist").show();
+        $(".two").hide();
+    });
+
+
+});
+
